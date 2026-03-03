5ec8b65c935140e8138bbebfb0952e6234f89131
Sofiia Kuzmenko <kuzmenkosofi@gmail.com>
2026-03-03 14:46:17 +0100

    add some file fot tasks

---
+++ Projekt01/Bool/zad2.py
@@ -0,0 +1,16 @@
+def trigon(a,b,c):
+    if a == 0 or b == 0 or c == 0:
+        return 'odcinek misi być większy od 0'
+    elif a == b and b == c:
+        return 'trójkąt jest równoboczny'
+    elif a == b or b == c:
+        return 'trójkąt jest równoramienny'
+    else:
+        return 'trójkąt jest różnoramienny'
+
+a = int(input('Podaj odcinek a: '))
+b = int(input('Podaj odcinek b: '))
+c = int(input('Podaj odcinek c: '))
+
+wynik = trigon(a,b,c)
+print(wynik)
---
+++ Projekt01/Numbers/zad1.py
@@ -0,0 +1,9 @@
+def rok(num):
+    if num % 4 == 0 or num % 400 == 0:
+        return f"rok {num} jest przęstepny"
+    else:
+        return 'rok nie jest przęstepny'
+
+rok_usr = int(input('Podaj rok: '))
+wynik = rok(rok_usr)
+print(wynik)
